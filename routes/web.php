648851ce5d8e58e7ce802862d5aaf66015e2bc8f
AlbertWebs <albertmuhatia@gmail.com>
2024-05-24 18:35:24 +0300

    update Footer

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/adult-diapers', [App\Http\Controllers\HomeController::class, 'adult'])->name('adult-diapers');
+Route::get('/baby-diapers', [App\Http\Controllers\HomeController::class, 'baby'])->name('baby-diapers');
+Route::get('/about-us', [App\Http\Controllers\HomeController::class, 'about'])->name('about-us');
+Route::get('/wholesale', [App\Http\Controllers\HomeController::class, 'wholesale'])->name('wholesale');
+
+Auth::routes();
